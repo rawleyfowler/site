3acaa20611423c0924b30f6cf2b72c0abe05f649
rawleyIfowler <rawleyIfowler@gmail.com>
2022-02-23 16:37:53 -0600

    fixed typo on startup script

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # WIP: Do not use this at the moment
 if [ -e "main" ]; then
-	./main.go &> /var/log/rawleyxyz/log
+	./main &> /var/log/rawleyxyz/log
 else
 	go build "main.go" && ./main &> /var/log/rawleyxyz/log
 fi
