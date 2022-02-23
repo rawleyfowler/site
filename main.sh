4d688d71d36694419ae020c416833f9428fba798
rawleyIfowler <rawleyIfowler@gmail.com>
2022-02-23 16:42:17 -0600

    piped stderr and stdout to stdout log file

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # WIP: Do not use this at the moment
 if [ -e "main" ]; then
-	./main &> /var/log/rawleyxyz/log
+	./main >> /var/log/rawleyxyz/log 2>&1
 else
 	go build "main.go" && ./main &> /var/log/rawleyxyz/log
 fi
