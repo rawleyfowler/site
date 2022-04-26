179c0196f10f19ff96f227ce1803f1d2cc542090
Rawley Fowler <75388349+rawleyfowler@users.noreply.github.com>
2022-04-26 15:19:31 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# WIP: Do not use this at the moment
+# This script is to be run by your init service, such as rc.d
+# WIP: Do not use this at the moment.
 if [ -e "main" ]; then
 	$1/main >> /var/log/rawleyxyz/log 2>&1
 else
-	go build "$1/main.go" && $1/main &> /var/log/rawleyxyz/log
+	go build "$1/main.go"
+	$1/main >> /var/log/rawleyxyz/log 2>&1
 fi
