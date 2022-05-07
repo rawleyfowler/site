d5799a888d7f7a39b8b92f80a905f97ab3261b53
rawleyfowler <rawleyfowler@gmail.com>
2022-05-07 11:24:31 -0500

    update init script

--- main.sh
+++ main.sh
@@ -2,8 +2,8 @@
 # This script is to be run by your init service, such as rc.d
 # WIP: Do not use this at the moment.
 if [ -e "main" ]; then
-	$1/main >> /var/log/rawleyxyz/log 2>&1
+	./rawleydotxyz >> /var/log/rawleydotxyz 2>&1
 else
 	go build "$1/main.go"
-	$1/main >> /var/log/rawleyxyz/log 2>&1
+	./rawleydotxyz >> /var/log/rawleydotxyz 2>&1
 fi
