ca63b3f967182972f51c87ab37003cef15bfd23a
Rawley Fowler <rawleyfowler@gmail.com>
2022-10-07 07:42:22 -0600

    ?

---
+++ render/index.eml.ml
@@ -0,0 +1,19 @@
+let render () =
+  <h3>Welcome!</h3>
+  <p>
+    Hi, I'm Rawley. I'm a software developer and self-proclaimed systems administrator
+    from Saskatchewan, Canada. I am experienced with a wide variety of technologies, including 
+    but not limited to OCaml, Clojure, Scheme, GNU/Linux, OpenBSD, C/C++,
+    and Java. I am particularily interested in functional programming and
+    programming language theory.
+  </p>
+  <p>
+    All of my personal projects are <a href="https://www.gnu.org/philosophy/philosophy.html">Free Software</a> and can be
+    found on <a href="https://git.rawley.xyz">my git server</a> or, on my <a href="https://github.com/rawleyfowler">github</a>. 
+    I spend most of my free time working on personal projects,
+    writing blog posts, or listening to bluegrass music.
+    To top it all off, I am a free software advocate, digital minimalist, and a full time husband.
+  </p>
+  <p>
+    If you need to contact me, please send me an email.
+  </p>
